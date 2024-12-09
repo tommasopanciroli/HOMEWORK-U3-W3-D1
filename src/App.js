36c34a2fbbc611688601cb57b4b3c239978ead1c
tommaso.panciroli <tommypanci@gmail.com>
2024-12-09 15:37:16 +0100

    adds star to save the job

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
-import MainSearch from "./components/MainSearch";
-import CompanySearchResults from "./components/CompanySearchResults";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+import MainSearch from './components/MainSearch'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import CompanySearchResults from './components/CompanySearchResults'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
         <Route path="/:company" element={<CompanySearchResults />} />
       </Routes>
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/Job.jsx
+++ src/components/Job.jsx
@@ -3,16 +3,19 @@ import { Link } from 'react-router-dom'
 
 const Job = ({ data }) => (
   <Row
-    className="mx-0 mt-3 p-3"
+    className="mx-0 mt-3 p-3 d-flex"
     style={{ border: '1px solid #00000033', borderRadius: 4 }}
   >
     <Col xs={3}>
       <Link to={`/${data.company_name}`}>{data.company_name}</Link>
     </Col>
-    <Col xs={9}>
+    <Col xs={9} className="d-flex align-items-center">
       <a href={data.url} target="_blank" rel="noreferrer">
         {data.title}
       </a>
+      <button className="ms-auto">
+        <i class="bi bi-star"></i>
+      </button>
     </Col>
   </Row>
 )
